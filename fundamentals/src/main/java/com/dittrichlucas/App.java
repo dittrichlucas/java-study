2361078e4a897b15a9dae2c768df1c9cd5a959bf
Lucas Dittrich <ld.dittrich@gmail.com>
2023-11-10 15:58:19 -0300

    first commit
    Signed-off-by: Lucas Dittrich <ld.dittrich@gmail.com>

---
+++ fundamentals/src/main/java/com/dittrichlucas/App.java
@@ -0,0 +1,13 @@
+package com.dittrichlucas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
